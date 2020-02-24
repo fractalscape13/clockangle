bd9f12845c836a3fff9242d1bf5b59e2ef84c249
Jamison Cozart and Joseph Wangemann <joseph@>
2020-02-24 11:28:01 -0800

    return degree differential between hour and minute hand, always giving the smaller angle

--- ClockAngle.cs
+++ ClockAngle.cs
@@ -8,11 +8,32 @@ class Program
     Console.WriteLine("Please enter a time in HR:MIN format");
     string inputTime = Console.ReadLine();
     int colonIndex = inputTime.IndexOf(":");
-    string hour = inputTime.Substring(0, colonIndex);
-    string minutes = inputTime.Substring(colonIndex + 1);
+    string hourStr = inputTime.Substring(0, colonIndex);
+    string minutesStr = inputTime.Substring(colonIndex + 1);
+    int hour = int.Parse(hourStr);
+    int minutes = int.Parse(minutesStr);
+    Clock newClock = new Clock(hour, minutes);
+    Console.WriteLine(newClock.checkDegrees());
   }
 }
 class Clock
 {
-
+  public int hourNum;
+  public int minuteNum;
+  public Clock(int hour, int minute)
+  {
+    hourNum = hour;
+    minuteNum = minute;
+  }
+  public int checkDegrees()
+  {
+    int hourDegrees = hourNum * 30;
+    int minuteDegrees = minuteNum * 6;
+    int diffDegrees = Math.Abs(hourDegrees - minuteDegrees);
+    if (diffDegrees > 180)
+    {
+      diffDegrees = 360 - diffDegrees;
+    }
+    return diffDegrees;
+  }
 }
